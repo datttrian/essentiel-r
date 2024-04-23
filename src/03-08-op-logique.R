4631f728c2f9b9fefe05b3ea9ee466f91aafd5a8
Dat Tran <datttrian@gmail.com>
2024-04-23 07:36:20 +0000

    Définir les relations d'ordre et les opérations logiques

---
+++ src/03-08-op-logique.R
@@ -0,0 +1,38 @@
+# Découverte des relations d’ordre ( > < <= >= ) et maitrise des opérations logiques ( & |)
+# relation d'ordre
+0 < 2
+0 > 2
+2 > 0
+0 <= 2
+2 >= 1
+0 == 1
+0 == 0
+1 != 0
+~ 1 != 1
+!TRUE
+!FALSE
+
+# algebre booleaenne
+0 | 1
+1 | 1
+1 & 1
+1 & 0
+
+# Utile dans les filtres
+## L'énigme de Sherlock Holmes
+## Sherlock Holmes enquête sur un meurtre. Il a trouvé une note avec le message suivant :
+## "Le meurtrier est un homme mesurant entre 170 et 180 cm,
+## il a entre 30 et 40 ans, il porte une barbe et un chapeau."
+## Sherlock Holmes a trouvé trois suspects :
+df <- data.frame(
+    c("Albert", "Jacques", "Jean"),
+    c(175, 178, 173),
+    c(35, 32, 41),
+    c(TRUE, FALSE, TRUE),
+    c(TRUE, TRUE, TRUE)
+)
+colnames(df) <- c("Prénom", "Taille", "Age", "Barbe", "Chapeau")
+
+## Qui est le coupable ?
+df[which(df$Barbe == TRUE & df$Chapeau == TRUE), ]
+df[which(df$Barbe == TRUE & df$Chapeau == TRUE & df$Age < 40), ]
