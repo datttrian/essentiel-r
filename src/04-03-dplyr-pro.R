754ac421c63d542c8f35245b9cd8fe1507040f9e
Dat Tran <datttrian@gmail.com>
2024-04-23 08:01:23 +0000

    Transformer des données avec dplyr

---
+++ src/04-03-dplyr-pro.R
@@ -0,0 +1,24 @@
+# Apprendre à manipuler et transformer des données avec {dplyr}
+library(dplyr)
+
+# select() : sélectionner des variables par leur nom ;
+select(iris, Sepal.Length, Sepal.Width)
+
+# rename() : renommer des variables ;
+a <- rename(iris, SL = Sepal.Length, SW = Sepal.Width)
+
+# filter() : sélectionner des observations selon une ou plusieurs conditions ;
+b <- filter(iris, Sepal.Length > 5)
+
+# arrange() : trier la table selon une ou plusieurs variables ;
+arrange(iris, Sepal.Length)
+
+# mutate() : ajouter des variables qui sont fonction d’autres variables ;
+c <- mutate(iris, ratio = Sepal.Length / Sepal.Width)
+
+# summarise() : calculer une statistique à partir de données ;
+summarise(iris, mean(Sepal.Length))
+
+# group_by() : faire des opérations par groupe.
+g <- group_by(iris, Species) %>%
+    summarise(mean(Sepal.Length))
