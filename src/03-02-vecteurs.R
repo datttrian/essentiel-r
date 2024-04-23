b2943a0eb02b1dacf09ef01efd148e37ac785b5a
Dat Tran <datttrian@gmail.com>
2024-04-23 06:54:55 +0000

    Gérer des vecteurs

---
+++ src/03-02-vecteurs.R
@@ -0,0 +1,44 @@
+# Présentation du concept de vecteur et première utilisation pratique avec des exemples concrets
+
+# c pour column
+x <- c(1, 3, 6, 9, 10)
+
+x <- 1:4
+x
+
+# ajout 2 à tout
+x + 2
+
+# multiplier tout par 2
+x * 2
+
+#
+t <- 1:7
+
+z <- x + t
+# erreur si pas la meme taille
+
+sum(x)
+
+cumsum(x)
+
+max(x)
+
+min(x)
+
+mean(x)
+
+var(x)
+
+sort(x)
+
+sort(x, decreasing = TRUE)
+
+length(x)
+
+# allez à la composante 3
+x[3]
+
+x[c(1:2)]
+
+x[c(1, 3, 4)]
