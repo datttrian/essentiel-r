3b6dba0313c33191dad1ff12969ad97d3958a4c4
Dat Tran <datttrian@gmail.com>
2024-04-23 06:02:55 +0000

    Utiliser les opérations mathématiques

---
+++ src/02-03-operations.R
@@ -0,0 +1,13 @@
+# Découverte de tous les opérateurs mathématiques disponibles
+
+# addition
+# soustraction
+# multiplication
+# division
+sqrt(4) -  2^(1/2)
+abs(-5)
+exp(1)
+log(6.38, base=exp(1))
+pi
+15 %/% 4
+factorial(5)
