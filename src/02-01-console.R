6e1ab61530f4cf4b4ca5d313b65d0bbe80518fa1
Dat Tran <datttrian@gmail.com>
2024-04-23 05:28:50 +0000

    Utiliser la console

---
+++ src/02-01-console.R
@@ -0,0 +1,24 @@
+# Explication sur le fonctionnement de la console en R et test des premières commandes
+
+# maitriser les 4 zones
+
+getwd()
+
+# utilisation de la console
+a <- "Sherlock"
+b <- "Moriaty"
+
+ls()
+
+rm(b)
+
+ls()
+
+#
+list.files()
+
+
+# graph
+demo(graphics)
+
+# créer un script R
